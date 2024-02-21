2e3fb54f8cc4fdbb6b8acbdc3246f3a683cf0be1
Soum <dev.web21@outlook.fr>
2024-02-21 17:15:48 +0100

    mise en place d'un element reutilisable pour affichage des informations du personnage selectionné (en cours)

--- script.js
+++ script.js
@@ -21,8 +21,6 @@ const prevClick = () => {
 
 }
 
-console.log(prevButton);
-
 function nextClick() {
     let active = document.querySelector('.active');
     let divNext = active.nextElementSibling
@@ -44,17 +42,45 @@ function nextClick() {
 
 // CARDS
 
-    let previewContainer = document.querySelector('.characters-preview');
+    let previewContainer = document.querySelector('.character-preview');
     let previewBox = previewContainer.querySelectorAll('.preview');
     
+    let dataTargets = [
+
+        {   target : 'c-1',
+
+            image : '/images/mao mao.png',
+            
+            name: 'Mao Mao', 
+        
+            description: `Fille d'un apothicaire vivant à la périphérie de la ville et âgée de 17 ans à peine, Mao Mao travaille au service de courtisanes dans le quartier des plaisirs. Sa vie bascule quand elle est kidnappée par des bandits qui la vendent au palais impérial. Elle est entraînée dans les intrigues du palais et grâce à ses connaissances insoupçonnables en matière de poisons et une curiosité insatiable pour tout ce qui y touche, elle est repérée par l'intendant de la cour, Jinshi. En sauvant la fille de la concubine de l'empereur, Gyokuyō, elle devient sa dame de compagnie et sa goûteuse officielle. `
+        },
+
+        {   target: 'c-2',
+            
+            name: 'Jinshi', 
+        
+            description: `Eunuque à la beauté surnaturelle, il officie à la cour intérieure où résident les concubines et leur personnel respectif en tant qu'intendant. Alors que son visage attire les hommes et les femmes du palais, il se trouve plutôt attiré par Mao Mao et adore la taquiner car c'est l'unique personne qui reste insensible à son charme.` },
+        
+    ];
+
     document.querySelectorAll('.characters-container .character').forEach(character =>{
       character.onclick = () =>{
-        previewContainer.style.display = 'flex';
+        
         let name = character.getAttribute('data-name');
-        previewBox.forEach(preview =>{
-          let target = preview.getAttribute('data-target');
-          if(name == target){
-            preview.classList.add('active');
+        console.log(name);
+        dataTargets.forEach(dataTarget =>{
+          if(name == dataTarget.target){
+            
+            // previewContainer.style.display = 'flex';
+            previewContainer.innerHTML =
+            `<div>
+            <i class="fas fa-times"></i>
+            <img src=${dataTarget.image} alt="">
+            <h3>${dataTarget.name}</h3>
+            <p>${dataTarget.description}</p>
+         </div>
+         `;
           }
         });
       };
@@ -73,6 +99,12 @@ function nextClick() {
 // MENU BURGER
 
 let buttonBurger = document.querySelector('.navbar-toggler');
+let navLinks = document.querySelector(".collapse.navbar-collapse");
+
+buttonBurger.addEventListener('click', () => {
+    console.log(navLinks);
+    navLinks.classList.toggle('active');
+});
 
 
 
